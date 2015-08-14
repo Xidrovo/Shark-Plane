9d1b278bf9981363cca6cdeca7b186587caf4fcb
Xidrovo <Xidrovo@hotmail.com>
2015-08-14 01:43:20 -0700

    Espero no dañar las cosas
    En serio D:!

---
+++ Shark-Plane/Assets/Scripts/Looper.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Looper : MonoBehaviour {
+
+	public GameObject PortalOut;
+	public GameObject MainCharacter, Boss, Camara;
+
+	private Vector3 Temp, TempMain, TempBoss;
+
+	void OnTriggerEnter2D(Collider2D Trigg)
+	{
+		Temp = Camara.transform.position;
+
+		TempMain.y = Temp.y - MainCharacter.transform.position.y;
+		TempMain.z = 0f;
+
+		Temp.x = PortalOut.transform.position.x;
+		Camara.transform.position = Temp;
+		TempMain.x =  MainCharacter.transform.position.x - Camara.transform.position.x;
+
+		TempBoss = Boss.transform.position;
+		TempBoss.x = PortalOut.transform.position.x;
+
+
+		MainCharacter.transform.position = TempMain;
+		Boss.transform.position = TempBoss;
+	}
+}
---
+++ Shark-Plane/Assets/Scripts/Tutorial/Testo.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Testo : MonoBehaviour {
+
+	private Text testo;
+	public static int Controlador = 0;
+
+	void Start()
+	{
+		testo = this.GetComponent<Text> ();
+		StartCoroutine(TestCoroutine());
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Controlador > 0) 
+		{
+			//Comenzar la rutina del tutorial.
+		}
+	}
+
+	IEnumerator TestCoroutine(){
+		testo.text = "¡hey!";
+		yield return new WaitForSeconds(1);
+		testo.text = "¿Como estas?";
+		yield return new WaitForSeconds(2);
+		testo.text = "Bienvenido, ¿estas emocionado?";
+		yield return new WaitForSeconds(2);
+		testo.text = "Muchos lo estan cuando pasan por aqui";
+		yield return new WaitForSeconds(3);
+		testo.text = "Primero lo primero";
+		yield return new WaitForSeconds(2);
+		testo.text = "Te doy la bienvenida a este lugar";
+		yield return new WaitForSeconds(2);
+		testo.text = "Ahora aprendamos los controles basicos";
+		yield return new WaitForSeconds(2);
+		testo.text = "¿Ves ese deslizador en el celular?";
+		yield return new WaitForSeconds(2);
+		testo.text = "Con el podras cambiar tu velocidad";
+		yield return new WaitForSeconds(2);
+		testo.text = "Vamos, trata de moverte hacia al frente";
+		yield return new WaitForSeconds(2);
+		testo.text = "Solo mueve el deslizador hacia al frente";
+		yield break;
+		Debug.Log ("You'll never see this"); // produces a dead code warning
+	}
+}
