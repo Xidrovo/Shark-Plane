d3b6f8fb5f3e0a921f195fab6783a85c4f0dc8f3
mavemore <veronica940413@hotmail.com>
2015-08-14 13:23:30 -0500

    Prefab de Fundamuertos
    Aun tengo que arreglar una cosa del script

--- Shark-Plane/Assets/Scripts/Villanos/Fundamuertos.cs
+++ Shark-Plane/Assets/Scripts/Villanos/Fundamuertos.cs
@@ -3,13 +3,63 @@ using System.Collections;
 
 public class Fundamuertos : MonoBehaviour {
 
+	private Atributos atri;
+	private bool Subir;
+	private bool AmpliarVuelo;
+	private float CorteAmplitud;
+	private float AltoMax;
+	private float Amplitud;
+	
 	// Use this for initialization
 	void Start () {
-	
+		Subir = true;
+		AmpliarVuelo = false;
+		CorteAmplitud = 1f;
+		atri = GetComponent<Atributos> ();
+		AltoMax = atri.AlturaMax;
+		Amplitud = atri.Rango;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Subir) {
+			BottomUp();
+			if((int)(this.gameObject.transform.position.y)==(int)((int)(AltoMax))){
+				Subir = false;
+			}
+		} else {
+			TopDown();
+			if((int)(this.gameObject.transform.position.y)==(int)(AltoMax - Amplitud)){
+				Subir = true;
+			}
+		}
+		if(AmpliarVuelo){
+			CorteAmplitud += Time.deltaTime;
+			AltoMax = atri.AlturaMax + (atri.AlturaMax*((int)(CorteAmplitud)/2f));
+		}else{
+			CorteAmplitud -= Time.deltaTime;
+			AltoMax = atri.AlturaMax - (atri.AlturaMax/((int)(CorteAmplitud)*2f));
+		}
+		Amplitud = atri.Rango /((int)(CorteAmplitud));
+		if (CorteAmplitud >3f) {
+			CorteAmplitud = 3f;
+			AmpliarVuelo = false;
+		} else if (CorteAmplitud <1f) {
+			CorteAmplitud = 1f;
+			AmpliarVuelo = true;
+		}
+		MoveHorizontal ();
+	}
 	
+	void BottomUp(){
+		transform.Translate (Vector3.up * atri.Speedy * Time.deltaTime/CorteAmplitud);
+	}
+	
+	void TopDown(){
+		transform.Translate (Vector3.down * atri.Speedy * Time.deltaTime/CorteAmplitud);
+	}
+	
+	void MoveHorizontal(){
+		transform.Translate (Vector3.right * atri.Speedx * Time.deltaTime);
 	}
 }
