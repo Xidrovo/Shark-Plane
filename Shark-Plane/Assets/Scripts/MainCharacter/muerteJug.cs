c5bdc330c989d317b3f4edfdeecc573ec14a11b6
firefoxfenix <christiancarriespi@Gmail.com>
2015-08-18 14:32:36 -0500

    choque jugador, balas senoidal probar

---
+++ Shark-Plane/Assets/Scripts/MainCharacter/muerteJug.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class muerteJug : MonoBehaviour {
+
+	// Use this for initialization
+	private int bd=0;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (bd == 1) 
+		{
+			this.gameObject.SetActive(false);
+			bd=0;
+		}
+		for (int i=0; i<10; i++) 
+		{
+			if((i+1)==10)
+			{
+				this.gameObject.SetActive(true);
+			}
+		}
+	
+	}
+
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if (!col.gameObject.CompareTag ("Bala") ) 
+		{
+			bd=1;
+			//bajar vida
+
+		}
+		
+	}
+
+
+}
---
+++ Shark-Plane/Assets/Scripts/Villanos/ataqBoss.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ataqBoss : MonoBehaviour {
+
+	public GameObject villano, prefab;
+	private GameObject nuevo;
+	public Quaternion rotacion = Quaternion.Euler(0,0,0);
+	private Atributos Temp;
+	// Use this for initialization
+	void Start () {
+		Temp = this.GetComponent<Atributos> ();
+		villano = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ataqSenoidal()
+	{
+		for(int i=0; i<360; i++)
+		{
+			int ran=Random.Range(0,360);
+			if(i<(ran+10)|| (i>ran))
+			{
+				Disparar(i);
+			}
+
+		}
+	}
+	public void Disparar(int i)
+	{
+		nuevo = (GameObject)Instantiate (prefab, 10.5f* Mathf.Cos(2*Mathf.PI*i), rotacion);
+		nuevo.GetComponent<Rigidbody2D> ().AddRelativeForce ((new Vector2 (-Temp.Speed, 0)), ForceMode2D.Impulse);
+	}
+}
