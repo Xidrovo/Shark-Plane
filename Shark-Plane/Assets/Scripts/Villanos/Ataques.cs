75a07ad6d4339b8c92307b3837accaebdbd83119
mavemore <veronica940413@hotmail.com>
2015-08-20 00:23:00 -0500

    Tanktuga + Detector de distancia hacia sharkplane
    Ya está el prefab y como el script era muy parecido al de ataques, mejor
    modifiqué este. Y también las balas tienen gravedad o sino se iban por
    la eternidad hacia arriba.

--- Shark-Plane/Assets/Scripts/Villanos/Ataques.cs
+++ Shark-Plane/Assets/Scripts/Villanos/Ataques.cs
@@ -5,26 +5,41 @@ public class Ataques : MonoBehaviour {
 
 	// Use this for initialization
 	public GameObject villano, prefab;
-	private GameObject nuevo;
+	private GameObject nuevo, shark;
 	public Quaternion rotacion = Quaternion.Euler(0,0,0);
-
+	public bool vertical;
+	public float shootForce;
+	
 	private Atributos Temp;
 	void Start () {
 		Temp = this.GetComponent<Atributos> ();
 		villano = this.gameObject;
+		shark = GameObject.FindWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
-
-		Disparar ();
-
-	
+		if (SharkCerca()) {
+			if(vertical){
+				LanzarProyectil();
+			}else Disparar ();
+		}
 	}
 	public void Disparar()
 	{
 		nuevo = (GameObject)Instantiate (prefab, (villano.transform.position), rotacion);
 		nuevo.GetComponent<Rigidbody2D> ().AddRelativeForce ((new Vector2 (-Temp.Speed, 0)), ForceMode2D.Impulse);
+	}
+	
+	void LanzarProyectil(){
+		nuevo = (GameObject)Instantiate (prefab, new Vector2(villano.transform.position.x,villano.transform.position.y+0.5f), rotacion);
+		nuevo.GetComponent<Rigidbody2D> ().AddRelativeForce (new Vector2 (1f, shootForce), ForceMode2D.Impulse);
+	}
+	
+	bool SharkCerca(){
+		if (Vector3.Distance(this.gameObject.transform.position,shark.transform.position) <= 8f)
+			return true;
+		return false;
 	}
 }
