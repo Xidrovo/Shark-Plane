e0006d4a04f8bfbc77f79afefddf34554fa10f4c
firefoxfenix <christiancarriespi@Gmail.com>
2015-08-20 19:45:27 -0500

    balas jugador

---
+++ Shark-Plane/Assets/Scripts/Bullets And Specials/explo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.CompareTag("MainCamera"))
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
